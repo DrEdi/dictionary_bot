76521e92464441a4af494b67841ffc9fac07a146
DrEdi <grigoriy.rubel@gmail.com>
2017-10-01 20:36:19 +0300

    Add utils for user logic

---
+++ utils.py
@@ -0,0 +1,23 @@
+import shelve
+
+
+def set_user_game(chat_id, estimated_answer):
+    with shelve.open('shelve') as storage:
+        storage[str(chat_id)] = estimated_answer
+
+
+def finish_user_game(chat_id):
+    with shelve.open('shelve') as storage:
+        try:
+        	del storage[str(chat_id)]
+        except KeyError:
+        	pass
+
+
+def get_answer_for_user(chat_id):
+    with shelve.open('shelve') as storage:
+        try:
+            answer = storage[str(chat_id)]
+            return answer
+        except KeyError:
+            return None
